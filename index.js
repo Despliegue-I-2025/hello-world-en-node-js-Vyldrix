33fa59ed714d18b088fc0ba3c41f0073ced05a94
Lauti-08 <alejandroloyola0803@gmail.com>
2025-08-18 21:24:58 -0300

    Rama Docker

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ const server = createServer((req, res) => {
 res.end('Hola Mundo\n');
 });
 
-server.listen(3000, () => {
-  console.log('Servidor escuchando en http://localhost:3000');
+server.listen(3002, '0.0.0.0', () => {
+console.log('Servidor escuchando en http://localhost:3002docker');
 });
